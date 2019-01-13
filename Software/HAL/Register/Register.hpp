61c10f331ace7947caa052614b66f2ce9e100b93
Pawel Warzecha <ProrokWielki@o2.pl>
2019-01-13 15:50:18 +0100

    Added Register template class

--- Software/HAL/Register/Register.hpp
+++ Software/HAL/Register/Register.hpp
@@ -1,8 +1,23 @@
-/*
- * Register.hpp
+/**
+  *  @file Register.hpp
+  *
+  *  @brief Wrapper for the Registers.
+  *
+  *  Created on: 16-11-2018
+  *  @author: Pawe≈Ç Warzecha
+  */
+
+/**
+ *  @brief Static class for operations on registers.
  *
- *  Created on: 16-11-2018
- *      Author: Pawe≥ Warzecha
+ *  @details
+ *  Class allows for the basic operations on the registers,
+ *  such as changing value of field, getting its value and toggling
+ *  bit in register.
+ *
+ *  The class is static, it has a private constructor and can't be instantiated.
+ *
+ *  @tparam REGISTER_ADDRESS address of the register
  */
 
 #ifndef HAL_REGISTER_REGISTER_HPP_
@@ -10,45 +25,83 @@
 
 #include <stdint.h>
 
-template<const uint32_t REGISTER_ADDRESS, uint8_t FIELD_BIT_LENGTH>
-class Register
-{
-public:
-	static void set_value(uint32_t u32Value, uint8_t u8FieldNumber)
-	{
-		u32Register = ( (u32Register & ~(get_mask() << (u8FieldNumber * FIELD_BIT_LENGTH))) | (u32Value << (u8FieldNumber * FIELD_BIT_LENGTH)) );
-	}
+template<const uint32_t REGISTER_ADDRESS>
+  class Register
+  {
+  public:
 
-	static uint32_t get_value(uint8_t u8FieldNumber)
-	{
-		return (u32Register & (get_mask() << (u8FieldNumber * FIELD_BIT_LENGTH)));
-	}
+    /** Sets given values to register, without changing other values.
+     *
+     * @param u32Value Value to be written to the register
+     * @param u8FielStartPosition Position in register from where to start writing the value
+     * @param u8FieldBitLength Length in bits of the field to which the value is written.
+     */
+    static void set_value(uint32_t u32Value, uint8_t u8FielStartPosition, uint8_t u8FieldBitLength = 1)
+    {
+      u32Register = ( ( u32Register & ~( get_mask(u8FieldBitLength) << ( u8FielStartPosition) ) )
+                    | ( u32Value << ( u8FielStartPosition) ) );
+    }
 
-	static void toggle_bit(uint32_t u8Position)
-	{
-		u32Register ^= (1U << u8Position);
-	}
-protected:
+    /** Reads value from the register.
+     *
+     * @param u8FielStartPosition Position in register from where to start reading the value
+     * @param u8FieldBitLength Length in bits of the field from which the value is read.
+     *
+     * @return Value of wanted field in register.
+     */
+    static uint32_t get_value(uint8_t u8FielStartPosition, uint8_t u8FieldBitLength = 1)
+    {
+      return ( u32Register & ( get_mask(u8FieldBitLength) << ( u8FielStartPosition) ) );
+    }
 
-private:
-	Register(){}
+    /** Toggles single bit in register.
+     *
+     * @param u8Position Position of bit to be toggled.
+     */
+    static void toggle_bit(uint8_t u8Position)
+    {
+      u32Register ^= ( 1U << u8Position );
+    }
 
-	static uint32_t get_mask()
-	{
-		uint32_t u32Mask = 0;
-		uint8_t iterator = FIELD_BIT_LENGTH;
+    static void set_bit(uint8_t u8Position)
+    {
+      u32Register |= ( 1U << u8Position );
+    }
 
-		while(iterator--)
-			u32Mask = ((u32Mask << 1U) | 1U);
+    static void clear_bit(uint8_t u8Position)
+    {
+      u32Register &= ~( 1U << u8Position );
+    }
 
-		return u32Mask;
-	}
+  protected:
 
-	static uint32_t & u32Register;
+  private:
 
-};
+    /// Constructor, it is private so the class can't be instantiated.
+    Register(){}
 
-template<const uint32_t REGISTER_ADDRESS, uint8_t FIELD_BIT_LENGTH>
-	uint32_t & Register<REGISTER_ADDRESS, FIELD_BIT_LENGTH>::u32Register = *((uint32_t *)REGISTER_ADDRESS);
+    /** Generates a bit mask.
+     *
+     * @param u8FieldBitLength length of the mask to be generated.
+     *
+     * @return mask of length @param u8FieldBitLength starting form LSB.
+     */
+    static uint32_t get_mask(uint8_t u8FieldBitLength)
+    {
+      uint32_t u32Mask = 0;
+
+      while(u8FieldBitLength--)
+        u32Mask = ( ( u32Mask << 1U ) | 1U );
+
+      return u32Mask;
+    }
+
+    /// Reference to register value.
+    static uint32_t & u32Register;
+
+  };
+
+template<const uint32_t REGISTER_ADDRESS>
+  uint32_t & Register<REGISTER_ADDRESS>::u32Register = *( (uint32_t *)REGISTER_ADDRESS );
 
 #endif /* HAL_REGISTER_REGISTER_HPP_ */
