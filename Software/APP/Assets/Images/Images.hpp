5978b5f69216b10f84b010ad2bc6b57ab4c8409d
Pawel Warzecha <ProrokWielki@o2.pl>
2019-02-24 10:51:05 +0100

    Added missing assets

---
+++ Software/APP/Assets/Images/Images.hpp
@@ -0,0 +1,16 @@
+/*
+ * Images.hpp
+ *
+ *  Created on: Feb 15, 2019
+ *      Author: pawel
+ */
+
+#ifndef APP_ASSETS_IMAGES_IMAGES_HPP_
+#define APP_ASSETS_IMAGES_IMAGES_HPP_
+
+#include <stdint.h>
+
+extern uint8_t * Mario_pixel_map;
+
+
+#endif /* APP_ASSETS_IMAGES_IMAGES_HPP_ */
