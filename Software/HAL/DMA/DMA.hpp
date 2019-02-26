f7e8eba207b86a4078eba96d48f1e584345f659b
Pawel Warzecha <ProrokWielki@o2.pl>
2019-02-26 19:26:51 +0100

    added HAL DMA module

--- Software/HAL/DMA/DMA.hpp
+++ Software/HAL/DMA/DMA.hpp
@@ -1,9 +1,9 @@
 /**
-  * DMA.hpp
-  *
-  *  Created on: 13-11-2018
-  *  @author: Paweł Warzecha
-  */
+ * DMA.hpp
+ *
+ *  Created on: 13-11-2018
+ *  @author: Paweł Warzecha
+ */
 
 #ifndef HAL_DMA_DMA_HPP_
 #define HAL_DMA_DMA_HPP_
@@ -15,27 +15,213 @@
 
 #include "DMA_types.hpp"
 
+constexpr static uint8_t MEMORY_TRANSFER_SIZE_FIELD_BIT_LENGTH = 16;
+constexpr static uint8_t PERIPHERAL_ADDRESS_FIELD_BIT_LENGTH = 32;
+constexpr static uint8_t MEMORY_ADDRESS_FIELD_BIT_LENGTH = 32;
 constexpr static uint8_t PRIORITY_LEVEL_FIELD_BIT_LENGTH = 2;
+constexpr static uint8_t PERIHERAL_SIZE_FIELD_BIT_LENGTH = 2;
+constexpr static uint8_t MEMORY_SIZE_FIELD_BIT_LENGTH = 2;
 
-template<const uint32_t DMA_CHANNEL_ADDRESS>
-  class DMA
-  {
-  public:
+/**
+ *  @brief Static class for operations on DMAs.
+ *
+ *  @details
+ *  Class allows configuration and simple usage of the DMA.
+ *
+ *  The class is static, it has a private constructor and can't be instantiated.
+ *
+ *  @tparam DMA_CHANNEL_ADDRESS address of the DMA channel.
+ */
+template <const uint32_t DMA_CHANNEL_ADDRESS>
+class DMA
+{
+public:
+    /**
+     * @brief Sets priority for the DMA channel.
+     *
+     * @param eDMAPriority priority to be set.
+     */
+    static void set_priority(DMAPriority_t eDMAPriority)
+    {
+        CCR::set_value(eDMAPriority, DMA_CCR_PL_Pos, PRIORITY_LEVEL_FIELD_BIT_LENGTH);
+    }
 
-  static void set_priority(DMAPriority_t eDMAPriority)
-  {
-    CCR::set_value(eDMAPriority, DMA_CCR_PL_Pos, PRIORITY_LEVEL_FIELD_BIT_LENGTH);
-  }
+    /**
+     * @brief Enables memory-to-memory DMA transfer.
+     */
+    static void enable_mem2mem()
+    {
+        CCR::set_bit(DMA_CCR_MEM2MEM_Pos);
+    }
+    /**
+     * @brief Disables memory-to-memory DMA transfer.
+     */
+    static void disable_mem2mem()
+    {
+        CCR::clear_bit(DMA_CCR_MEM2MEM_Pos);
+    }
 
-  protected:
+    /**
+     * @brief Sets size of the memory data.
+     *
+     * @param eDMAMemorySize size of the data
+     */
+    static void set_memory_size(DMASize_t eDMAMemorySize)
+    {
+        CCR::set_value(eDMAMemorySize, DMA_CCR_MSIZE_Pos, MEMORY_SIZE_FIELD_BIT_LENGTH);
+    }
 
-  private:
+    /**
+     * @brief Sets size of the peripheral data.
+     *
+     * @param eDMAPeripheralSize size of the data
+     */
+    static void set_peripheral_size(DMASize_t eDMAPeripheralSize)
+    {
+        CCR::set_value(eDMAPeripheralSize, DMA_CCR_PSIZE_Pos, PERIHERAL_SIZE_FIELD_BIT_LENGTH);
+    }
+
+    /**
+     * @brief Enables automatic memory increment.
+     */
+    static void enable_memory_increment()
+    {
+        CCR::set_value(eEnable, DMA_CCR_MINC_Pos);
+    }
+    /**
+     * @brief Disables automatic memory increment.
+     */
+    static void disable__memory_incremen()
+    {
+        CCR::set_value(eDisable, DMA_CCR_MINC_Pos);
+    }
+
+    /**
+     * @brief Enables automatic peripheral increment.
+     */
+    static void enable_peripheral_increment()
+    {
+        CCR::set_value(eEnable, DMA_CCR_PINC_Pos);
+    }
+    /**
+     * @brief Disables automatic peripheral increment.
+     */
+    static void disable_peripheral_incremen()
+    {
+        CCR::set_value(eDisable, DMA_CCR_PINC_Pos);
+    }
+
+    /**
+     * @brief Enables circular mode.
+     */
+    static void enable_circular_mode()
+    {
+        CCR::set_value(eEnable, DMA_CCR_CIRC_Pos);
+    }
+    /**
+     * @brief Disables circular mode.
+     */
+    static void disable_circular_mode()
+    {
+        CCR::set_value(eDisable, DMA_CCR_CIRC_Pos);
+    }
+
+    /**
+     * @brief Sets from where the data is read.
+     * @param eDMAReadDirection direction of read.
+     */
+    static void set_read_direction(DMAReadDirection_t eDMAReadDirection)
+    {
+        CCR::set_value(eDMAReadDirection, DMA_CCR_DIR_Pos);
+    }
+
+    /**
+     * @brief Enables interrupt.
+     *
+     * @param DMAInterrupt interrupt to be enabled.
+     */
+    static void enable_interrupt(DMAInterrupts_t DMAInterrupt)
+    {
+        CCR::set_bit(DMAInterrupt);
+    }
+    /**
+     * @brief Disables interrupt.
+     *
+     * @param DMAInterrupt interrupt to be disabled.
+     */
+    static void disable_interrupt(DMAInterrupts_t DMAInterrupt)
+    {
+        CCR::clear_bit(DMAInterrupt);
+    }
+
+    /**
+     * @brief Enables DMA channel.
+     */
+    static void enable()
+    {
+        CCR::set_bit(DMA_CCR_EN_Pos);
+    }
+    /**
+     * @brief Disables DMA channel.
+     */
+    static void disable()
+    {
+        CCR::clear_bit(DMA_CCR_EN_Pos);
+    }
+
+    /**
+     * @brief Sets the number of transfers.
+     *
+     * @param TransferSize number of transfer to be made.
+     */
+    static void set_transfer_size(uint16_t TransferSize)
+    {
+        CNDTR::set_value(TransferSize, DMA_CNDTR_NDT_Pos, MEMORY_TRANSFER_SIZE_FIELD_BIT_LENGTH);
+    }
+
+    /**
+     * @brief Sets address of the peripheral.
+     *
+     * @param PeripheralAddress peripheral address.
+     */
+    static void set_periphearl_address(uint32_t PeripheralAddress)
+    {
+        CPAR::set_value(PeripheralAddress, DMA_CPAR_PA_Pos, PERIPHERAL_ADDRESS_FIELD_BIT_LENGTH);
+    }
+
+    /**
+     * @brief Sets address of the memory.
+     *
+     * @param MemoryAddress memory address.
+     */
+    static void set_memory_address(uint32_t MemoryAddress)
+    {
+        CMAR::set_value(MemoryAddress, DMA_CMAR_MA_Pos, MEMORY_ADDRESS_FIELD_BIT_LENGTH);
+    }
+
+    /**
+     * @brief Sets DMA request.
+     *
+     * @param DMARequest DMA request to be enabled.
+     */
+    static void set_request(DMARequest_t DMARequest)
+    {
+        CMAR::set_value(DMARequest, DMA_CMAR_MA_Pos, MEMORY_ADDRESS_FIELD_BIT_LENGTH);
+    }
+
+protected:
+private:
     DMA(){};
 
-    constexpr volatile static uint32_t adrCCR = (uint32_t)(&(((DMA_Channel_TypeDef *) DMA_CHANNEL_ADDRESS)->CCR));
+    constexpr volatile static uint32_t adrCCR = (uint32_t)(&(((DMA_Channel_TypeDef *)DMA_CHANNEL_ADDRESS)->CCR));
+    constexpr volatile static uint32_t adrCNDTR = (uint32_t)(&(((DMA_Channel_TypeDef *)DMA_CHANNEL_ADDRESS)->CNDTR));
+    constexpr volatile static uint32_t adrCPAR = (uint32_t)(&(((DMA_Channel_TypeDef *)DMA_CHANNEL_ADDRESS)->CPAR));
+    constexpr volatile static uint32_t adrCMAR = (uint32_t)(&(((DMA_Channel_TypeDef *)DMA_CHANNEL_ADDRESS)->CMAR));
 
     typedef Register<adrCCR> CCR;
-
+    typedef Register<adrCNDTR> CNDTR;
+    typedef Register<adrCPAR> CPAR;
+    typedef Register<adrCMAR> CMAR;
 };
 
 #endif /* HAL_DMA_DMA_HPP_ */
--- Software/HAL/DMA/DMA_types.hpp
+++ Software/HAL/DMA/DMA_types.hpp
@@ -1,54 +1,65 @@
 /**
-  * DMA_types.hpp
-  *
-  *  Created on: 30-11-2018
-  *  @author: Paweł Warzecha
-  */
+ * DMA_types.hpp
+ *
+ *  Created on: 30-11-2018
+ *  @author: Paweł Warzecha
+ */
 
 #ifndef HAL_DMA_DMA_TYPES_HPP_
 #define HAL_DMA_DMA_TYPES_HPP_
 
+/**
+ * @brief DMA priorities
+ */
 typedef enum
 {
-  eMemeoryToMemory,
-  eMemmoryToPeriphery,
-  ePeripheryToMemory,
-  ePeripheryToPeriphery
-}DMATransferType_t;
+    eDMALow,      //!< eDMALow low priority
+    eDMAMedium,   //!< eDMAMedium medium priority
+    eDMAHigh,     //!< eDMAHigh high priority
+    eDMAVeryHigh  //!< eDMAVeryHigh very high priority
+} DMAPriority_t;
 
+/**
+ * @brief DMA data size.
+ */
 typedef enum
 {
-  eLow,
-  eMedium,
-  eHigh,
-  eVeryHigh
-}DMAPriority_t;
+    e8Bits,   //!< e8Bits 8 bits data size
+    e16Bits,  //!< e16Bits 16 bits data size
+    e32Bits   //!< e32Bits 32 bits data size
+} DMASize_t;
 
+/**
+ * @brief DMA requests.
+ */
 typedef enum
 {
-  e8Bits,
-  e16Bits,
-  e32Bits
-}DMAMemeorySize_t;
+    eRequest0,  //!< eRequest0 request 0
+    eRequest1,  //!< eRequest1 request 1
+    eRequest2,  //!< eRequest2 request 2
+    eRequest3,  //!< eRequest3 request 3
+    eRequest4,  //!< eRequest4 request 4
+    eRequest5,  //!< eRequest5 request 5
+    eRequest6,  //!< eRequest6 request 6
+    eRequest7   //!< eRequest7 request 7
+} DMARequest_t;
 
+/**
+ * @brief Read direciton
+ */
 typedef enum
 {
-  e8Bits,
-  e16Bits,
-  e32Bits
-}DMAPeripherySize_t;
+    eReadFromPeriphearl,  //!< eReadFromPeriphearl read for peripheral
+    eReadFromMemory       //!< eReadFromMemory read from memory
+} DMAReadDirection_t;
 
+/**
+ * @brief DMA interrupts
+ */
 typedef enum
 {
-  eMemoryIncrement = DMA_CCR_MINC_Pos,
-  ePeripheryIncrement = DMA_CCR_PINC_Pos,
-  eCircularMode = DMA_CCR_CIRC_Pos
-}DMAOptions_t;
-
-typedef enum
-{
-  eTransferError = DMA_CCR_TEIE_Pos,
-  eHalfTransfer = DMA_CCR_HTIE_Pos,
-  eTransferCompleted = DMA_CCR_TCIE_Pos
-}DMAInterrupts_t;
+    eTransferError = DMA_CCR_TEIE_Pos,     //!< eTransferError transfer error interrupt
+    eHalfTransfer = DMA_CCR_HTIE_Pos,      //!< eHalfTransfer half transfer interrupt
+    eTransferCompleted = DMA_CCR_TCIE_Pos  //!< eTransferCompleted transfer complete interrupt
+} DMAInterrupts_t;
 #endif /* HAL_DMA_DMA_TYPES_HPP_ */
