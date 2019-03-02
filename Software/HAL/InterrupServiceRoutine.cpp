ef2e57ac9a108376544793ba95835d80da89c678
Pawel Warzecha <ProrokWielki@o2.pl>
2019-03-02 23:08:28 +0100

    using new DMA interrupt callback function

--- Software/HAL/InterrupServiceRoutine.cpp
+++ Software/HAL/InterrupServiceRoutine.cpp
@@ -13,27 +13,19 @@ namespace ISR
 {
 void DMA1_Channel2_IRQHandler(void)
 {
-    HAL::PB13::toogle_output_value();
-    DMA1->IFCR |= ((1 << 6) | (1 << 4) | (1 << 5) | (1 << 7));
-    NVIC_ClearPendingIRQ(DMA1_Channel2_IRQn);
+    HAL::DMA1_2::transfer_complete_callback();
 }
 void DMA1_Channel4_IRQHandler(void)
 {
-    HAL::PB13::toogle_output_value();
-    DMA1->IFCR |= ((1 << 12) | (1 << 13) | (1 << 14) | (1 << 15));
-    NVIC_ClearPendingIRQ(DMA1_Channel4_IRQn);
+    HAL::DMA1_4::transfer_complete_callback();
 }
-void DMA1_Channel6_IRQHandler(void)
+void DMA2_Channel7_IRQHandler(void)
 {
-    HAL::PB13::toogle_output_value();
-    DMA1->IFCR |= ((1 << 20) | (1 << 21) | (1 << 22) | (1 << 23));
-    NVIC_ClearPendingIRQ(DMA1_Channel6_IRQn);
+    HAL::DMA2_7::transfer_complete_callback();
 }
 void DMA2_Channel2_IRQHandler(void)
 {
-    HAL::PB13::toogle_output_value();
-    DMA2->IFCR |= ((1 << 6) | (1 << 4) | (1 << 5) | (1 << 7));
-    NVIC_ClearPendingIRQ(DMA2_Channel2_IRQn);
+    HAL::DMA2_2::transfer_complete_callback();
 }
 }  // namespace ISR
 }  // namespace ISR
