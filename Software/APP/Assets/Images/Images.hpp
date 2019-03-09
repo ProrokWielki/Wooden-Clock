670f32c3dc7d45acaa3a8c404daf4859fe3c14bf
Pawel Warzecha <ProrokWielki@o2.pl>
2019-03-09 12:58:47 +0100

    New images

--- Software/APP/Assets/Images/Images.hpp
+++ Software/APP/Assets/Images/Images.hpp
@@ -14,4 +14,26 @@ extern uint8_t Mario_pixel_map[];
 extern uint8_t Arrows1[];
 extern uint8_t Arrows2[];
 
+extern uint8_t frame_1[];
+extern uint8_t frame_2[];
+extern uint8_t frame_3[];
+extern uint8_t frame_4[];
+extern uint8_t frame_5[];
+extern uint8_t frame_6[];
+extern uint8_t frame_7[];
+extern uint8_t frame_8[];
+extern uint8_t frame_9[];
+extern uint8_t frame_10[];
+extern uint8_t frame_11[];
+extern uint8_t frame_12[];
+extern uint8_t frame_13[];
+extern uint8_t frame_14[];
+extern uint8_t frame_15[];
+extern uint8_t frame_16[];
+extern uint8_t frame_17[];
+extern uint8_t frame_18[];
+extern uint8_t frame_19[];
+extern uint8_t frame_20[];
+extern uint8_t frame_21[];
+
 #endif /* APP_ASSETS_IMAGES_IMAGES_HPP_ */
