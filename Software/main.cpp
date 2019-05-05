58b8615a69755a9c968b0d36a351a7e809d7f44e
Pawel Warzecha <ProrokWielki@o2.pl>
2019-05-05 11:49:00 +0200

    cleanup

--- Software/main.cpp
+++ Software/main.cpp
@@ -1,8 +1,11 @@
+#include <cmath>
 #include <stdint.h>
 
 #include "APP.hpp"
 #include "BSP.hpp"
 #include "HAL.hpp"
+
+#include "DRV/LSM9DS1/LSM9DS1.hpp"
 
 #include "Assets/Images/Images.hpp"
 
@@ -12,9 +15,16 @@ int main(void)
     HAL::init();
     BSP::init();
 
+
+
+
+    //    HAL::LSM9DS1_1.write_magneto(0x22, 0b10000000);
+    //    for (uint16_t i = 0; i < 0xffff; i++)
+    //        ;
+
     BSP::display.set_frame_buffer(Arrows1);
 
-    while (HAL::BUTTON2.get_input_value() == eLow or HAL::BUTTON.get_input_value() == eLow or HAL::BUTTON3.get_input_value() == eLow or
+    while (HAL::BUTTON2.get_input_value() == eLow and HAL::BUTTON.get_input_value() == eLow and HAL::BUTTON3.get_input_value() == eLow and
            HAL::BUTTON4.get_input_value() == eLow)
     {
         ;
