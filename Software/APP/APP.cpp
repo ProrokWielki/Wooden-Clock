c7e5f7cedf38f84223861b80a42ffe2c3a8341c1
Pawel Warzecha <ProrokWielki@o2.pl>
2020-05-03 13:13:47 +0200

    possible to conter with ESP, dipslay task called more frequently

--- Software/APP/APP.cpp
+++ Software/APP/APP.cpp
@@ -10,6 +10,8 @@
 
 #include "BSP.hpp"
 
+#include <GUI.hpp>
+
 #include "DataContainer.hpp"
 
 [[noreturn]] void display_task(void * pvParameters)
@@ -21,21 +23,8 @@
     for (;;)
     {
 
-        BSP::buton1.update();
-        BSP::buton2.update();
-        BSP::buton3.update();
-        BSP::buton4.update();
-
-        if (BSP::buton1.wasPressed())
-            DataContainer::stateMachine.signal_callback(Signal::BUTTON1);
-        if (BSP::buton2.wasPressed())
-            DataContainer::stateMachine.signal_callback(Signal::BUTTON2);
-        if (BSP::buton3.wasPressed())
-            DataContainer::stateMachine.signal_callback(Signal::BUTTON3);
-        if (BSP::buton4.wasPressed())
-            DataContainer::stateMachine.signal_callback(Signal::BUTTON4);
-
         DataContainer::stateMachine.update();
+
         vTaskDelay(xDelay);
     }
 }
@@ -48,14 +37,43 @@
 
     for (;;)
     {
-        DataContainer::stateMachine.update();
+        BSP::buton1.update();
+        BSP::buton2.update();
+        BSP::buton3.update();
+        BSP::buton4.update();
+
+        if (BSP::buton1.wasPressed() || HAL::up)
+        {
+            DataContainer::stateMachine.signal_callback(Signal::BUTTON1);
+            HAL::up = false;
+        }
+        if (BSP::buton2.wasPressed() || HAL::down)
+        {
+            DataContainer::stateMachine.signal_callback(Signal::BUTTON2);
+            HAL::down = false;
+        }
+        if (BSP::buton3.wasPressed() || HAL::left)
+        {
+            DataContainer::stateMachine.signal_callback(Signal::BUTTON3);
+            HAL::left = false;
+        }
+        if (BSP::buton4.wasPressed() || HAL::right)
+        {
+            DataContainer::stateMachine.signal_callback(Signal::BUTTON4);
+            HAL::right = false;
+        }
         vTaskDelay(xDelay);
     }
 }
 
 void APP_init()
 {
+
+    BSP::display.set_frame_buffer(DataContainer::stateMachine.getFrameBuffer());
+    BSP::display.set_dispaly_redrawn_callback([&]() { DataContainer::stateMachine.swap_framebuffers(); });
+
     xTaskCreate(display_task, "dispaly_task", 2048, NULL, 2, NULL);
+    xTaskCreate(system_interface_task, "system_interface_task", 64, NULL, 1, NULL);
 
     vTaskStartScheduler();
 }
