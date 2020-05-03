1f44941e39b22bcd7f495f96e4753e50898dea3b
Pawel Warzecha <ProrokWielki@o2.pl>
2020-05-03 13:08:53 +0200

    Gyroscope place holder

---
+++ Software/BSP/Gyroscope/Gyroscope.hpp
@@ -0,0 +1,15 @@
+/**
+  * Gyroscope.hpp
+  *
+  *  Created on: 02-05-2019
+  *  @author: Paweł Warzecha
+  */
+
+#ifndef BSP_GYROSCOPE_GYROSCOPE_HPP_
+#define BSP_GYROSCOPE_GYROSCOPE_HPP_
+
+
+
+
+
+#endif /* BSP_GYROSCOPE_GYROSCOPE_HPP_ */
