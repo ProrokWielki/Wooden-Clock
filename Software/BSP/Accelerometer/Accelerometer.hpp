2477e1e7ad649df12f5b96b7efc914565a5742e3
Pawel Warzecha <ProrokWielki@o2.pl>
2020-05-03 13:08:41 +0200

    Accelerometer place holder

---
+++ Software/BSP/Accelerometer/Accelerometer.hpp
@@ -0,0 +1,15 @@
+/**
+  * Accelerometer.hpp
+  *
+  *  Created on: 02-05-2019
+  *  @author: Paweł Warzecha
+  */
+
+#ifndef BSP_ACCELEROMETER_ACCELEROMETER_HPP_
+#define BSP_ACCELEROMETER_ACCELEROMETER_HPP_
+
+
+
+
+
+#endif /* BSP_ACCELEROMETER_ACCELEROMETER_HPP_ */
