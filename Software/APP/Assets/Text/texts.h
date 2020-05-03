f50865500a75307683aecc2a7095cb8d9be02920
Pawel Warzecha <ProrokWielki@o2.pl>
2020-05-03 11:41:14 +0200

    added texts

---
+++ Software/APP/Assets/Text/texts.h
@@ -0,0 +1,18 @@
+#ifndef TEXTS_H
+#define TEXTS_H
+
+#include <stdint.h>
+
+extern uint8_t hour_13_57[];
+extern uint8_t one[];
+extern uint8_t two[];
+extern uint8_t three[];
+extern uint8_t four[];
+extern uint8_t five[];
+extern uint8_t six[];
+extern uint8_t seven[];
+extern uint8_t eight[];
+extern uint8_t nine[];
+extern uint8_t zero[];
+
+#endif /* TEXTS_H */
