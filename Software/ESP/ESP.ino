b0660f05e07e3da557fe558a1c55362c43fd9172
ProrokWielki <ProrokWielki@o2.pl>
2021-04-18 12:17:51 +0200

    moved ESP to Software/ESP

--- Software/ESP/ESP.ino
+++ Software/ESP/ESP.ino
@@ -10,6 +10,7 @@
 #include "helpers/mqtt.h"
 #include "helpers/ota.h"
 
+WiFiServer server(8750);
 WiFiClient wifi_;
 PubSubClient mqtt_(mqtt::server_ip, mqtt::server_port, wifi_);
 
@@ -17,6 +18,7 @@ std::map<std::string, std::function<void(byte * payload, unsigned int length)>> 
 {"up", [](byte * payload, unsigned int length) { Serial.print("u"); }},
 {"down", [](byte * payload, unsigned int length) { Serial.print("d"); }},
 {"left", [](byte * payload, unsigned int length) { Serial.print("l"); }},
+{"reset", [](byte * payload, unsigned int length) { Serial.print("w"); }},
 {"right", [](byte * payload, unsigned int length) { Serial.print("r"); }}};
 
 void callback(char * topic, byte * payload, unsigned int length)
@@ -45,10 +47,34 @@ void setup()
     mqtt_.setCallback([](char * topic, byte * payload, unsigned int length) { handle_topic[topic](payload, length); });
 
     mqtt::subscribe(mqtt_, handle_topic);
+    server.begin();
+    Serial.println("----- setup done --------");
 }
 
 void loop()
 {
     ota::handle();
     mqtt_.loop();
-}
+
+  //check if there are any new clients
+  if (server.hasClient()){
+      //find free/disconnected spot
+      if (!wifi_ || !!wifi_.connected()){
+        if(wifi_) wifi_.stop();
+        wifi_ = server.available();
+        Serial.print("New client: ");
+        Serial.print(" : Client data : ");
+      }
+    }
+    //no free/disconnected spot so reject
+  //check clients for data
+    if (wifi_ && wifi_.connected()){
+      if(wifi_.available()){
+        //get data from the telnet client and push it to the UART
+        while(wifi_.available()) Serial.print(wifi_.read(),HEX);
+        Serial.println("");
+      }
+    }
+
+  }
+
