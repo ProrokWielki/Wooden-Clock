8f2b05fb3c60b822c8bb72b02141d98d1fdf3e3b
ProrokWielki <ProrokWielki@o2.pl>
2025-10-19 14:56:02 +0000

    uncommented gpio test

--- Software/STM/tests/hil/gpio_test/gpio_test.cpp
+++ Software/STM/tests/hil/gpio_test/gpio_test.cpp
@@ -18,79 +18,81 @@ RegisterTest(GpioTest, SetAsOutput, [](Test * test) {
     EXPECT_EQ(GPIOC->MODER & (3 << 18), 1 << 18);
 });
 
-// RegisterTest(RegisterTest, SetValue, [](Test * test) {
-//     Register rcc_apb1enr1{&(RCC->APB1ENR1)};
+RegisterTest(RegisterTest, SetValue, [](Test * test) {
+    Register rcc_apb1enr1{&(RCC->APB1ENR1)};
 
-//     RCC->APB1ENR1 = 0;
-//     EXPECT_EQ(RCC->APB1ENR1, 0);
+    RCC->APB1ENR1 = 0;
+    EXPECT_EQ(RCC->APB1ENR1, 0);
 
-//     rcc_apb1enr1.set_value(0b1'0101, 21, 4);  // set one bit over the size
+    rcc_apb1enr1.set_value(0b1'0101, 21, 4);  // set one bit over the size
 
-//     EXPECT_EQ(RCC->APB1ENR1, 0b0101 << 21);  // check if it wasn't set
-// });
+    EXPECT_EQ(RCC->APB1ENR1, 0b0101 << 21);  // check if it wasn't set
+});
 
-// RegisterTest(RegisterTest, GetBit, [](Test * test) {
-//     Register exti_imr1{&(EXTI->IMR1)};
+RegisterTest(RegisterTest, GetBit, [](Test * test) {
+    Register exti_imr1{&(EXTI->IMR1)};
 
-//     EXPECT_EQ(EXTI->IMR1, 0xFF820000);
+    EXPECT_EQ(EXTI->IMR1, 0xFF820000);
 
-//     EXPECT_EQ(exti_imr1.get_bit(15), 0);
-//     EXPECT_EQ(exti_imr1.get_bit(23), 1);
-// });
+    EXPECT_EQ(exti_imr1.get_bit(15), 0);
+    EXPECT_EQ(exti_imr1.get_bit(23), 1);
+});
 
-// RegisterTest(RegisterTest, SetBit, [](Test * test) {
-//     Register rcc_ahb2enr{&(RCC->AHB2ENR)};
+RegisterTest(RegisterTest, SetBit, [](Test * test) {
+    Register rcc_ahb2enr{&(RCC->AHB2ENR)};
 
-//     RCC->AHB2ENR = 0;
-//     EXPECT_EQ(RCC->AHB2ENR, 0);
+    RCC->AHB2ENR = 0;
+    EXPECT_EQ(RCC->AHB2ENR, 0);
 
-//     rcc_ahb2enr.set_bit(18);
-//     EXPECT_EQ(RCC->AHB2ENR, 1 << 18);
-// });
+    rcc_ahb2enr.set_bit(18);
+    EXPECT_EQ(RCC->AHB2ENR, 1 << 18);
+});
 
-// RegisterTest(RegisterTest, ClearBit, [](Test * test) {
-//     Register rcc_ahb2enr{&(RCC->AHB2ENR)};
+RegisterTest(RegisterTest, ClearBit, [](Test * test) {
+    Register rcc_ahb2enr{&(RCC->AHB2ENR)};
 
-//     RCC->AHB2ENR = 4;
-//     EXPECT_EQ(RCC->AHB2ENR, 4);
+    RCC->AHB2ENR = 4;
+    EXPECT_EQ(RCC->AHB2ENR, 4);
 
-//     rcc_ahb2enr.clear_bit(2);
-//     EXPECT_EQ(RCC->AHB2ENR, 0);
-// });
+    rcc_ahb2enr.clear_bit(2);
+    EXPECT_EQ(RCC->AHB2ENR, 0);
+});
 
-// RegisterTest(RegisterTest, GetAddress, [](Test * test) {
-//     Register rcc_ahb2enr{&(RCC->AHB2ENR)};
+RegisterTest(RegisterTest, GetAddress, [](Test * test) {
+    Register rcc_ahb2enr{&(RCC->AHB2ENR)};
 
-//     EXPECT_EQ(reinterpret_cast<uint32_t>(&(RCC->AHB2ENR)), rcc_ahb2enr.get_address());
-// });
+    EXPECT_EQ(reinterpret_cast<uint32_t>(&(RCC->AHB2ENR)), rcc_ahb2enr.get_address());
+});
 
-// RegisterTest(RegisterTest, ToogleBit, [](Test * test) {
-//     Register rcc_ahb2enr{&(RCC->AHB2ENR)};
+RegisterTest(RegisterTest, ToogleBit, [](Test * test) {
+    Register rcc_ahb2enr{&(RCC->AHB2ENR)};
 
-//     RCC->AHB2ENR = 0;
-//     EXPECT_EQ(RCC->AHB2ENR, 0);
+    RCC->AHB2ENR = 0;
+    EXPECT_EQ(RCC->AHB2ENR, 0);
 
-//     rcc_ahb2enr.toggle_bit(18);
-//     EXPECT_EQ(RCC->AHB2ENR, 1 << 18);
+    rcc_ahb2enr.toggle_bit(18);
+    EXPECT_EQ(RCC->AHB2ENR, 1 << 18);
 
-//     rcc_ahb2enr.toggle_bit(18);
-//     EXPECT_EQ(RCC->AHB2ENR, 0);
-// });
+    rcc_ahb2enr.toggle_bit(18);
+    EXPECT_EQ(RCC->AHB2ENR, 0);
+});
 
-// RegisterTest(RegisterTest, Read, [](Test * test) {
-//     Register can_fmr{&(CAN->FMR)};
+RegisterTest(RegisterTest, Read, [](Test * test) {
+    Register can_fmr{&(CAN->FMR)};
 
-//     EXPECT_EQ(CAN->FMR, CAN_FMR_RESET_VALUE);
-//     EXPECT_EQ(can_fmr.read(), CAN_FMR_RESET_VALUE);
-// });
+    constexpr static uint32_t CAN_FMR_RESET_VALUE{0x2A1C0E01};
 
-// RegisterTest(RegisterTest, Write, [](Test * test) {
-//     Register rcc_apb1enr1{&(RCC->APB1ENR1)};
+    EXPECT_EQ(CAN->FMR, CAN_FMR_RESET_VALUE);
+    EXPECT_EQ(can_fmr.read(), CAN_FMR_RESET_VALUE);
+});
 
-//     RCC->APB1ENR1 = 0;
-//     EXPECT_EQ(RCC->APB1ENR1, 0);
+RegisterTest(RegisterTest, Write, [](Test * test) {
+    Register rcc_apb1enr1{&(RCC->APB1ENR1)};
 
-//     rcc_apb1enr1.write(705537);
+    RCC->APB1ENR1 = 0;
+    EXPECT_EQ(RCC->APB1ENR1, 0);
 
-//     EXPECT_EQ(RCC->APB1ENR1, 705537);
-// });
+    rcc_apb1enr1.write(705537);
+
+    EXPECT_EQ(RCC->APB1ENR1, 705537);
+});
